19d68a0082202fd7fc2687758a231494b16629c8
y-tj <y-tj@10.0.52.49>
2017-03-29 16:18:36 +0800

    java 8

--- src/main/java/com/example/HelloLambda.java
+++ src/main/java/com/example/HelloLambda.java
@@ -6,41 +6,57 @@ import java.util.List;
 import java.util.function.Function;
 
 public class HelloLambda {
-  public static void main(String[] args) {
-    String[] strs = {"hello", "world", "what", "hi"};
-    Arrays.sort(strs, new LengthComparator());
-    for (String str : strs) {
-      System.out.println(str);
-    }
+	
+	public static void main(String[] args) {
+		String[] strs = { "hello", "world", "what", "hi" };
+		sortp(strs);
+		/*Arrays.sort(strs, new LengthComparator());
+		for (String str : strs) {
+			System.out.println(str);
+		}
 
-    Comparator<String> c1  = (String s1, String s2) -> Integer.compare(s1.length(), s2.length());
-    Comparator<String> c2  = (String s1, String s2) -> s1.compareTo(s2);
-    Comparator<String> c3  = (s1, s2) -> s2.compareTo(s1);
-    Comparator<String> c4 = String::compareTo;
+		Comparator<String> c1 = (String s1, String s2) -> Integer.compare(s1.length(), s2.length());
+		Comparator<String> c2 = (String s1, String s2) -> s1.compareTo(s2);
+		Comparator<String> c3 = (s1, s2) -> s2.compareTo(s1);
+		Comparator<String> c4 = String::compareTo; 
+		Arrays.sort(strs, (s1, s2) -> s1.compareTo(s2));
+		Arrays.sort(strs, String::compareToIgnoreCase);
+		// Arrays.sort(strs, c3);
+		for (String str : strs) {
+			System.out.println(str);
+		}
 
-    Arrays.sort(strs, (s1, s2) -> s2.compareTo(s1));
-    Arrays.sort(strs, String::compareToIgnoreCase);
-//    Arrays.sort(strs, c3);
-    for (String str : strs) {
-      System.out.println(str);
-    }
+		List<Integer> lst = Arrays.asList(new Integer[] { 3, 6, 4 });
+		lst.forEach(System.out::println);
+		System.out.println(transform("Daniel", String::toLowerCase));*/
+	}
 
-    List<Integer> lst = Arrays.asList(new Integer[]{3,6,4});
-    lst.forEach(System.out::println);
-    System.out.println(transform("Daniel", String::toLowerCase));
-  }
+	public static String transform(String s, Function<String, String> f) {
+		return f.apply(s);
+	}
 
-  public static String transform(String s, Function<String, String> f) {
-    return f.apply(s);
-  }
-
-  public static <T> int mapSum(List<T> entries, Function<T, Integer> f) {
-    int sum = 0;
-    for (T entry: entries) {
-      sum += f.apply(entry);
-    }
-    return sum;
-  }
+	public static <T> int mapSum(List<T> entries, Function<T, Integer> f) {
+		int sum = 0;
+		for (T entry : entries) {
+			sum += f.apply(entry);
+		}
+		return sum;
+	}
+	
+	public static void sortp(String [] strs){
+//		Comparator<String> c1 = (String s1, String s2) -> Integer.compare(s1.length(), s2.length());
+		Comparator<String> c3 = (s1, s2) -> s2.compareTo(s1);
+		Comparator<String> c4 = (s1, s2) -> s1.compareTo(s2);
+//		Arrays.sort(strs, (s1, s2) -> s1.compareTo(s2));
+		Arrays.sort(strs, c4);
+		for (String str : strs) {
+			System.out.println(str);
+		}
+		Arrays.sort(strs, c3);
+		for (String str : strs) {
+			System.out.println(str);
+		}
+	}
 }
 
 // https://docs.oracle.com/javase/tutorial/java/javaOO/lambdaexpressions.html
